a7d79c1f77ec2331bddc4b403d1296b27d1cfe59
Stelios Modes <smodes@thoughtworks.com>
2017-10-19 00:36:58 +0200

    Example of merging streams

--- src/test/scala/com/stelmod/akka/streams/MergeSourceTest.scala
+++ src/test/scala/com/stelmod/akka/streams/MergeSourceTest.scala
@@ -1,13 +1,14 @@
 package com.stelmod.akka.streams
 
-import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream._
-import akka.stream.scaladsl.{Flow, GraphDSL, RunnableGraph, Sink, Source, Zip}
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 import akka.stream.scaladsl.GraphDSL.Implicits._
-import akka.stream.testkit.TestSubscriber
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Sink, Source, Zip}
 import akka.stream.testkit.scaladsl.TestSink
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class MergeSourceTest extends WordSpec with Matchers with BeforeAndAfterAll {
   implicit var system: ActorSystem = _
@@ -38,5 +39,22 @@ class MergeSourceTest extends WordSpec with Matchers with BeforeAndAfterAll {
         .request(2)
         .expectNext((1, 101))
         .expectNext((2, 102))
+  }
+
+  "Merge Sources" in {
+    val source1 = NumbersSource.source(1, 3)
+    val source2 = NumbersSource.source(101, 103)
+
+    val zipStream = GraphDSL.create() { implicit builder =>
+      val m = builder.add(Merge[Int](2))
+      source1 ~> m
+      source2 ~> m
+
+      SourceShape.of(m.out)
+    }
+
+    val future = Source.fromGraph(zipStream).take(10).runWith(Sink.seq)
+    val result = Await.result(future, 1 seconds)
+    result should contain theSameElementsAs Iterable(1, 2, 3, 101, 102, 103)
   }
 }
