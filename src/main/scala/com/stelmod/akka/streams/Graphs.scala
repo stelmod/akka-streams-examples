1a303984e592b310bd6409d0aa73e64079ea85d0
Stelios Modes <smodes@thoughtworks.com>
2017-11-07 03:41:03 +0100

    Add examples to demonstrate backpressure

--- src/main/scala/com/stelmod/akka/streams/Graphs.scala
+++ src/main/scala/com/stelmod/akka/streams/Graphs.scala
@@ -1,21 +1,20 @@
 package com.stelmod.akka.streams
 
 import akka.NotUsed
-import akka.stream.{FlowShape, SourceShape}
+import akka.stream._
 import akka.stream.scaladsl.GraphDSL.Implicits._
-import akka.stream.scaladsl.{GraphDSL, Source}
+import akka.stream.scaladsl.{Broadcast, GraphDSL, Sink, Source}
+import scala.concurrent.duration._
+
 
 object Graphs {
   def evenAndGreater(source: Source[Int, NotUsed], lowerLimit: Int) = {
     GraphDSL.create() { implicit builder =>
-
-      val s = builder.add(source).out
-      val f1 = builder.add(Flows.even)
       val f2 = builder.add(Flows.largerThan(lowerLimit))
 
-      s ~> f1 ~> f2
+      source ~> Flows.even ~> f2
 
-      SourceShape.of(f2.out)
+      SourceShape(f2.out)
     }
   }
 
@@ -23,7 +22,21 @@ object Graphs {
     GraphDSL.create() { implicit builder =>
 
       val flow = builder.add(Flows.largerThan(lowerLimit))
-      FlowShape.of(flow.in, flow.out)
+      FlowShape(flow.in, flow.out)
+    }
+  }
+
+  def slowSink(source: Source[Int, NotUsed]) = {
+    GraphDSL.create() { implicit builder =>
+      val flow = Flows.even.delay(10 seconds, DelayOverflowStrategy.backpressure).addAttributes(Attributes.inputBuffer(1, 1))
+      val f2 = builder.add(flow)
+
+      val broadcast = builder.add(Broadcast[Int](2))
+      source ~> broadcast.in
+      broadcast.out(0) ~> Sink.foreach[Int](println)
+      broadcast.out(1) ~> f2.in
+
+      SourceShape(f2.out)
     }
   }
 }
---
+++ src/test/scala/com/stelmod/akka/streams/BackpressureTest.scala
@@ -0,0 +1,59 @@
+package com.stelmod.akka.streams
+
+import java.util.concurrent.TimeoutException
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.Attributes.InputBuffer
+import akka.stream.scaladsl.{RunnableGraph, Sink, Source}
+import akka.stream.{ActorMaterializer, Attributes, DelayOverflowStrategy}
+import akka.stream.testkit.scaladsl.TestSink
+import org.scalatest.concurrent.PatienceConfiguration.Timeout
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+
+import scala.collection.immutable
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
+class BackpressureTest extends WordSpec with Matchers with BeforeAndAfterAll {
+  implicit var system: ActorSystem = _
+  implicit var materializer: ActorMaterializer = _
+
+  override def beforeAll() {
+    system = ActorSystem("actors")
+    materializer = ActorMaterializer()
+  }
+
+  "Delayed Flow" in {
+    val source = NumbersSource.source(1, 10)
+    val delayedSource: Source[Int, NotUsed] = source.via(Flows.even)
+      .delay(1 seconds, DelayOverflowStrategy.backpressure)
+
+    val future: Future[immutable.Seq[Int]] = delayedSource.take(10).runWith(Sink.seq)
+
+    ScalaFutures.whenReady(future, Timeout(2 seconds)) {
+      result => result should contain only (2, 4, 6, 8, 10)
+    }
+  }
+
+  "Backpressure is visible when input buffer is small enough" in {
+    val source = NumbersSource.source(1, 10)
+    val flow = Flows.even
+
+    val delayedSource: Source[Int, NotUsed] = source.via(flow)
+      .delay(1 seconds, DelayOverflowStrategy.backpressure).addAttributes(Attributes.inputBuffer(1, 1))
+
+    val future: Future[immutable.Seq[Int]] = delayedSource.take(10).runWith(Sink.seq)
+
+    an [TimeoutException] should be thrownBy Await.result(future, 2 seconds)
+  }
+
+  "Backpressure stops the source from producing" in {
+    val source = NumbersSource.source(1, 10)
+    val graph = Graphs.slowSink(source)
+
+    val future = Source.fromGraph(graph).take(10).runWith(Sink.seq)
+    an [TimeoutException] should be thrownBy Await.result(future, 25 seconds)
+  }
+}
